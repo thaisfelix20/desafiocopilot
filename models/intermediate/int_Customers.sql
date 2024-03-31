8d808d57d7b7d47a11f521e2ba95c61617f1d355
Thais Santos <thais.santos@consulting-for.edenred.com>
2024-03-31 16:19:00 -0300

    Treating customers

--- models/intermediate/int_Customers.sql
+++ models/intermediate/int_Customers.sql
@@ -3,5 +3,16 @@ customers as (
     select *
     from {{ ref('stg_customers') }}
 )
+, treatingCustomers as (
+    select
+        staff_id
+        , (first_name || ' ' || last_name) as customerName
+        , email as emailCustomer
+        , phone as phoneCustomer
+        , active
+        , store_id
+        , manager_id
+    from customers
+)
 SELECT *
-from customers
+from treatingCustomers
